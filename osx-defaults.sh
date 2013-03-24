6c2ab7b66cee7d528fcd16c5533d045a6add30c2
Kim Pepper <kim@pepper.id.au>
2013-03-24 12:05:23 +1100

    Don't set computer name

--- osx-defaults.sh
+++ osx-defaults.sh
@@ -13,10 +13,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "Leto"
-sudo scutil --set HostName "leto"
-sudo scutil --set LocalHostName "leto"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "MathBook-Pro"
+# sudo scutil --set ComputerName "Leto"
+# sudo scutil --set HostName "leto"
+# sudo scutil --set LocalHostName "leto"
+# sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "MathBook-Pro"
 
 # Menu bar: disable transparency
 defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
